bd16f3a61a9756207846897dc51b3f3cf673511a
SheilaDriele <sheiladriele@hotmail.com>
2024-05-08 17:06:27 -0300

    Criacao da CaracteristicaEntity

---
+++ src/main/java/com/sheiladev/ebambu/entity/CaracteristicaEntity.java
@@ -0,0 +1,67 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "caracteristica")
+public class CaracteristicaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String valor;
+    @Column(name = "unidade_de_medida")
+    private String unidadeMedida;
+
+    public CaracteristicaEntity() {
+    }
+
+    public CaracteristicaEntity(Long id, String nome, String valor, String unidadeMedida) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    @Override
+    public String toString() {
+        return "CaracteristicaEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", valor='" + valor + '\'' +
+                ", unidadeMedida='" + unidadeMedida + '\'' +
+                '}';
+    }
+}
