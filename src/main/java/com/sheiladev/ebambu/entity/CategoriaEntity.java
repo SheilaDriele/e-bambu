14c8af0d22f262a5f0bf0e8983b59c5157389950
SheilaDriele <sheiladriele@hotmail.com>
2024-05-08 17:17:57 -0300

    Criacao de CategoriaEntity

---
+++ src/main/java/com/sheiladev/ebambu/entity/CategoriaEntity.java
@@ -0,0 +1,55 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class CategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public CategoriaEntity() {
+    }
+
+    public CategoriaEntity(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
