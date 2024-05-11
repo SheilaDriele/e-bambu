566ed33a1ef8ba6fb46bfa2e90fd74705701dc3f
SheilaDriele <sheiladriele@hotmail.com>
2024-05-11 00:55:40 -0300

    Entidade Documento

---
+++ src/main/java/com/sheiladev/ebambu/entity/ProdutoSelecionadoEntity.java
@@ -0,0 +1,70 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "produto_selecionado")
+public class ProdutoSelecionadoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private OfertaEntity oferta;
+    @ManyToOne
+    private PromocaoEntity promocao;
+    private BigInteger quantidade;
+
+    public ProdutoSelecionadoEntity() {
+    }
+
+    public ProdutoSelecionadoEntity(Long id, OfertaEntity oferta, PromocaoEntity promocao, BigInteger quantidade) {
+        this.id = id;
+        this.oferta = oferta;
+        this.promocao = promocao;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OfertaEntity getOferta() {
+        return oferta;
+    }
+
+    public void setOferta(OfertaEntity oferta) {
+        this.oferta = oferta;
+    }
+
+    public PromocaoEntity getPromocao() {
+        return promocao;
+    }
+
+    public void setPromocao(PromocaoEntity promocao) {
+        this.promocao = promocao;
+    }
+
+    public BigInteger getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigInteger quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoSelecionadoEntity{" +
+                "id=" + id +
+                ", oferta=" + oferta +
+                ", promocao=" + promocao +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
---
+++ src/main/java/com/sheiladev/ebambu/entity/PromocaoEntity.java
@@ -0,0 +1,92 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "promocao")
+public class PromocaoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private BigDecimal valor;
+    @ManyToOne
+    private OfertaEntity oferta;
+    private LocalDateTime dataHoraInicio;
+    private LocalDateTime dataHoraFim;
+    private BigDecimal quantidade;
+
+    public PromocaoEntity() {
+    }
+
+    public PromocaoEntity(Long id, BigDecimal valor, OfertaEntity oferta, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim, BigDecimal quantidade) {
+        this.id = id;
+        this.valor = valor;
+        this.oferta = oferta;
+        this.dataHoraInicio = dataHoraInicio;
+        this.dataHoraFim = dataHoraFim;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public OfertaEntity getOferta() {
+        return oferta;
+    }
+
+    public void setOferta(OfertaEntity oferta) {
+        this.oferta = oferta;
+    }
+
+    public LocalDateTime getDataHoraInicio() {
+        return dataHoraInicio;
+    }
+
+    public void setDataHoraInicio(LocalDateTime dataHoraInicio) {
+        this.dataHoraInicio = dataHoraInicio;
+    }
+
+    public LocalDateTime getDataHoraFim() {
+        return dataHoraFim;
+    }
+
+    public void setDataHoraFim(LocalDateTime dataHoraFim) {
+        this.dataHoraFim = dataHoraFim;
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "PromocaoEntity{" +
+                "id=" + id +
+                ", valor=" + valor +
+                ", oferta=" + oferta +
+                ", dataHoraInicio=" + dataHoraInicio +
+                ", dataHoraFim=" + dataHoraFim +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
