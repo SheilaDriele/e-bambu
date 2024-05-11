9dea498d38d44687e727a1396fdf0ed33e553212
flaviodiminuto <flavio.diminuto@outlook.com>
2024-05-11 00:24:10 -0300

    Entidade TipoDocumentoEntity

---
+++ src/main/java/com/sheiladev/ebambu/entity/TipoDocumentoEntity.java
@@ -0,0 +1,44 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tipo_documento")
+public class TipoDocumentoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+
+    public TipoDocumentoEntity() {
+    }
+
+    public TipoDocumentoEntity(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoDocumentoEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
