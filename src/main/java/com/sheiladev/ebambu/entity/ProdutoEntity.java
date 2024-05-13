e06592adaefa98e50bbba1d0bf0774034ee1cf34
SheilaDriele <sheiladriele@hotmail.com>
2024-05-13 12:50:05 -0300

    Criacao da SubCategoriaEntity e mapeamento de ProdutoEntity

--- src/main/java/com/sheiladev/ebambu/entity/ProdutoEntity.java
+++ src/main/java/com/sheiladev/ebambu/entity/ProdutoEntity.java
@@ -1,24 +1,94 @@
 package com.sheiladev.ebambu.entity;
 
 import jakarta.persistence.*;
-import com.sheiladev.ebambu.model.Caracteristica;
-import com.sheiladev.ebambu.model.Categoria;
-import com.sheiladev.ebambu.model.SubCategoria;
 import java.util.List;
 
 @Entity
 @Table(name = "produto")
 public class ProdutoEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String codigo;
-   //@Column(name = "categoria_id")
-   //private Categoria categoria;
-   //@Column(name = "subcategoria_id")
-   //private SubCategoria subCategoria;
-   //private String descricao;
-   //@ManyToMany(mappedBy = "produto")
-   //private List<Caracteristica> caracteristicas;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private CategoriaEntity categoria;
+    @ManyToOne
+    @JoinColumn(name = "subcategoria_id")
+    private SubCategoriaEntity subCategoria;
+    private String descricao;
+    @ManyToMany
+    private List<CaracteristicaEntity> caracteristicas;
 
+    public ProdutoEntity() {
+    }
+
+    public ProdutoEntity(Long id, String codigo, CategoriaEntity categoria, SubCategoriaEntity subCategoria, String descricao, List<CaracteristicaEntity> caracteristicas) {
+        this.id = id;
+        this.codigo = codigo;
+        this.categoria = categoria;
+        this.subCategoria = subCategoria;
+        this.descricao = descricao;
+        this.caracteristicas = caracteristicas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public CategoriaEntity getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaEntity categoria) {
+        this.categoria = categoria;
+    }
+
+    public SubCategoriaEntity getSubCategoria() {
+        return subCategoria;
+    }
+
+    public void setSubCategoria(SubCategoriaEntity subCategoria) {
+        this.subCategoria = subCategoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<CaracteristicaEntity> getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(List<CaracteristicaEntity> caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoEntity{" +
+                "id=" + id +
+                ", codigo='" + codigo + '\'' +
+                ", categoria=" + categoria +
+                ", subCategoria=" + subCategoria +
+                ", descricao='" + descricao + '\'' +
+                ", caracteristicas=" + caracteristicas +
+                '}';
+    }
 }
---
+++ src/main/java/com/sheiladev/ebambu/entity/SubCategoriaEntity.java
@@ -0,0 +1,55 @@
+package com.sheiladev.ebambu.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sub_categoria")
+public class SubCategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public SubCategoriaEntity() {
+    }
+
+    public SubCategoriaEntity(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "SubCategoriaEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
